a314e3961fa17e406a82819fd52b530e74b780ce
Malandru <andre.mtzd07@gmail.com>
2019-05-21 17:18:59 -0500

    Arreglo jeje

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name= 'movierecomender',
-    version='0.0.3',
+    version='0.0.4',
     author='Andres Duenas',
     description='Recomends a movie given two files',
     url='https://github.com/Malandru/Recomender',
