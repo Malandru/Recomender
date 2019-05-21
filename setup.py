f50545cb234ec6636fd548d23b27b20fd4cc2370
Malandru <andre.mtzd07@gmail.com>
2019-05-21 17:48:59 -0500

    Ultima prueba

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import setuptools
 
 setuptools.setup(
     name= 'movierecomender',
-    version='0.0.4',
+    version='0.0.5',
     author='Andres Duenas',
     description='Recomends a movie given two files',
-    url='https://github.com/Malandru/Recomender',
+    url='https://github.com/Malandru/Recomender.git',
     packages=setuptools.find_packages()
 )
